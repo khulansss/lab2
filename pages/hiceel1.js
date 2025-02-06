09398ac2c508490d6c00441d314546c47e6dcf02
Khangal1 <davaakhangal@gmail.com>
2025-02-06 13:48:31 +0800

    comment

--- pages/hiceel1.js
+++ pages/hiceel1.js
@@ -10,8 +10,10 @@ export default function useStateHook() {
             <button className="border border-black m-3 p-2 " onClick={() => setCount("khulan")}>Click me</button>
             <button className="border border-black m-3 p-2 " onClick={() => setCount("")}>Click me</button>
             <p className="ml-3">{count}</p>
-            
+            <div className="w-20 h-30 bg-black"></div>
             
         </div>
+        
+        
     )
 };
