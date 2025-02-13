be5949638bdeae83e1f45ad93096d9aff936a56c
Khangal1 <davaakhangal@gmail.com>
2025-02-13 14:35:50 +0800

    02.12

--- pages/hiceel2.js
+++ pages/hiceel2.js
@@ -4,14 +4,64 @@ const useStateHook = () => {
   return (
     <div className={`flex space-x-4 w-full h-screen bg-${color}-800`}>
       <p>Ungu solih</p>
-      <button
+     
+      
+     
+      {color == "red" ? (<>
+        <button
         className="border bg-red-800 py-4 ps-6"
         onClick={() => {
-          setColor("red");
+          setColor("red")
         }}
       >
         Ulaan
-      </button>;
+      </button> <button
+        className="border bg-white py-4 ps-6"
+        onClick={() => {
+          setColor("white");
+        }}
+      >
+        clear
+      </button> 
+      </>) :
+      color == "blue" ? (<><button
+        className="border bg-blue-800 py-4 ps-6"
+        onClick={() => {
+          setColor("blue");
+        }}
+      >
+        Tsenher
+      </button><button
+        className="border bg-white py-4 ps-6"
+        onClick={() => {
+          setColor("white");
+        }}
+      >
+        clear
+      </button></>) : 
+      color == "green" ? (<> <button
+        className="border bg-green-800 py-4 ps-6"
+        onClick={() => {
+          setColor("green");
+        }}
+      >
+        Nogoon
+      </button>  <button
+        className="border bg-white py-4 ps-6"
+        onClick={() => {
+          setColor("white");
+        }}
+      >
+        clear
+      </button></> 
+      ): (<><button
+        className="border bg-red-800 py-4 ps-6"
+        onClick={() => {
+          setColor("red")
+        }}
+      >
+        Ulaan
+      </button>
       <button
         className="border bg-blue-800 py-4 ps-6"
         onClick={() => {
@@ -19,7 +69,7 @@ const useStateHook = () => {
         }}
       >
         Tsenher
-      </button>;
+      </button>
       <button
         className="border bg-green-800 py-4 ps-6"
         onClick={() => {
@@ -27,24 +77,10 @@ const useStateHook = () => {
         }}
       >
         Nogoon
-      </button>
-      {color == "red" ? null : <button
-        className="border bg-blue-800 py-4 ps-6"
-        onClick={() => {
-          setColor("blue");
-        }}
-      >
-        Tsenher
-      </button> }
+      </button></>)
+       }
 
-      <button
-        className="border bg-white py-4 ps-6"
-        onClick={() => {
-          setColor("white");
-        }}
-      >
-        clear
-      </button>
+     
       
     </div>
   );
